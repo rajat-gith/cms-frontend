33c28f9de8f4b822e2ae664757f796b9dd00fb6f
rajat-gith <rajatnayak1582002@gmail.com>
2025-05-27 19:40:15 +0530

    worked on basic folder structure setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+	return (
+		<div className={styles.page}>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+		</div>
+	);
+}
